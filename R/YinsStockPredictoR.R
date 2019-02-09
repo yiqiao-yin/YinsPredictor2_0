23bd4b73091fc37e64b162a9e840fdf1e89836e4
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2019-02-09 10:14:08 -0500

    Update YinsStockPredictoR.R

--- R/YinsStockPredictoR.R
+++ R/YinsStockPredictoR.R
@@ -1,12 +1,8 @@
 #' @title Predicts Stock Price Movement for Given Stock Symbol
 #' @description This package predicts whether the stock price at tommorow's market close would be higher or lower compared to today's closing place.
-#'
 #' @param symbol
-#'
 #' @return NULL
-#'
 #' @examples  stock_predict('AAPL')
-#'
 #' @export stock_predict
 #'
 #' # Define function
